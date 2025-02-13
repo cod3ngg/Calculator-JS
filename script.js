da390d621b46e3366dcaabe14512fc2d8c3acfc9
Lorenz <150222307+cod3ngg@users.noreply.github.com>
2025-02-13 14:23:45 +0000

    First Version of Calculator-JS, basic input and output of numbers, no functionality yet

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
 const mainDiv = document.getElementById('main-div');
+const outputField = document.getElementById('output-field');
+
+const btns = document.querySelectorAll(".button");
+const oprbtns = document.querySelectorAll(".oprbtn");
+
+let val;
+let inputVal;
+let hasOperand = false;
 
 
+ btns.forEach((button) => {
+    button.addEventListener("click", () => {
+        val = button.value;
+        console.log(val);
+        userInputVal(); 
+    });
+ });
+
+ oprbtns.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(val + " operand");
+        if(hasOperand === true){
+            Operate();
+        }else {
+            hasOperand = true;
+        }
+
+    });
+ });
+
+
+function userInputVal(){
+    if(inputVal === undefined){
+        inputVal = val;
+    }else {
+        inputVal += val;
+    }
+
+    outputField.textContent = inputVal;
+}
+
+
+function Operate(){
+    outputField.textContent = "";
+}
