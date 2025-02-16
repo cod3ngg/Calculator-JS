d234f97fe047176dcaaa29b20ed099bb03650bd0
Lorenz <150222307+cod3ngg@users.noreply.github.com>
2025-02-16 08:06:21 +0000

    Fixed display and calculator has full functionality, just need added features

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
+
 const mainDiv = document.getElementById('main-div');
 const outputField = document.getElementById('output-field');
 
-const btns = document.querySelectorAll(".button");
+const numbtns = document.querySelectorAll(".numbtn");
 const oprbtns = document.querySelectorAll(".oprbtn");
 
 let val;
+let firstVal;
+let secondVal;
+let nextVal = false;
+let oprVal;
 let inputVal;
+let outputVal;
 let hasOperand = false;
 
 
- btns.forEach((button) => {
+ numbtns.forEach((button) => {
     button.addEventListener("click", () => {
         val = button.value;
         console.log(val);
-        userInputVal(); 
+        userInputVal();
+        displayOutput();
     });
  });
 
  oprbtns.forEach((button) => {
     button.addEventListener("click", () => {
-        console.log(val + " operand");
-        if(hasOperand === true){
-            Operate();
-        }else {
-            hasOperand = true;
+        let opr = button.value;
+        inputVal = "";
+        val = opr;
+        hasOperand = true;
+       
+
+        console.log(opr + " operand");
+        
+        switch(opr){
+            case "equals":
+                val = "";
+                Operate();
+                break;
+            default:
+                outputField.textContent = outputVal + opr;
+                outputVal += opr;
+                console.log("Operation Triggered");
         }
 
+
+        oprVal = opr;
+        nextVal = true;
     });
  });
 
@@ -37,10 +59,49 @@ function userInputVal(){
         inputVal += val;
     }
 
-    outputField.textContent = inputVal;
+    if(nextVal === true){
+        secondVal = inputVal;
+    }else{
+        firstVal = inputVal;
+    }
 }
 
+function displayOutput(){
+    if(outputVal === undefined){
+        outputVal = val;
+    }else {
+        outputVal += val;
+    }
+
+    outputField.textContent = outputVal;
+}
 
 function Operate(){
-    outputField.textContent = "";
+    let result;
+
+    firstVal = Number(firstVal);
+    console.log(firstVal);
+    secondVal = Number(secondVal);
+    console.log(secondVal);
+
+    switch(oprVal) {
+        case "+":
+            result = firstVal + secondVal;
+            break;
+        case "-":
+            result = firstVal - secondVal;
+            break;
+        case "x":
+            result = firstVal * secondVal;
+            break;
+        case "/":
+            result = firstVal / secondVal;
+            break;
+        default:
+            console.log("Invalid operation");
+            result = null; // Assign a default value if no valid operation is found
+    }
+
+    outputField.textContent = result;
+    console.log(result);
 }
